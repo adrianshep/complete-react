c4f281f744f3e2708ca5d90e313a604870a9120e
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-11-07 15:02:07 -0500

    es6-arrow-function lesson challenge

--- complete-react-projects/indecision-app/src/playground/es6-arrow-function.js
+++ complete-react-projects/indecision-app/src/playground/es6-arrow-function.js
@@ -1,13 +1,32 @@
-function square(x) {
-  return x * x;
-};
-
-console.log(square(3));
-
-// const squareArrow = (x) => {
+// function square(x) {
 //   return x * x;
 // };
+//
+// console.log(square(3));
+//
+// // const squareArrow = (x) => {
+// //   return x * x;
+// // };
+//
+// const squareArrow = (x) => x * x;
+//
+// console.log(squareArrow(4));
 
-const squareArrow = (x) => x * x;
+// const fullName = 'Andrew Mead';
+// let firstName;
+//
+// if (fullName) {
+//   firstName = fullName.split(' ')[0];
+//   console.log(firstName);
+// }
 
-console.log(squareArrow(4));
+// const fullName = 'Andrew Mead';
+// let firstName;
+
+let firstName = (fullName) => {
+    return fullName.split(' ')[0];
+  }
+
+let firstName = (fullName) => fullName.split(' ')[0];
+
+// console.log(firstName('Jen Mead'));
