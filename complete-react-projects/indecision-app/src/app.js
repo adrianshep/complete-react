ca5ebc2c5ddb82795831fad5a0db8d511b0cff57
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-11-12 13:06:05 -0500

    forms and inputs lesson challenge

--- complete-react-projects/indecision-app/src/app.js
+++ complete-react-projects/indecision-app/src/app.js
@@ -14,25 +14,31 @@ console.log('App.js is running!');
     if (option) {
       app.options.push(option);
       e.target.elements.option.value = '';
+      // call render function here after options array is added to
+      renderOptionsApp();
     }
   };
 
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      {app.subtitle && <p>{app.subtitle}</p>}
-      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-      <p>{app.options.length}</p>
-      <ol>
-        <li>Item one</li>
-        <li>Item two</li>
-      </ol>
-      <form onSubmit={onFormSubmit}>
-        <input type="text" name="option"/>
-        <button>Add Option</button>
-      </form>
-    </div>
-  );
+const renderOptionsApp = () => {
+    const template = (
+      <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+        <p>{app.options.length}</p>
+        <ol>
+          <li>Item one</li>
+          <li>Item two</li>
+        </ol>
+        <form onSubmit={onFormSubmit}>
+          <input type="text" name="option"/>
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
 
-const appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+  const appRoot = document.getElementById('app');
+  ReactDOM.render(template, appRoot);
+};
+// and call render function here
+renderOptionsApp();
