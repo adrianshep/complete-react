a1e4e43fc122aa4ebeda0aaf8bce49caab66ce3f
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-11-14 15:38:29 -0500

    arrays in JSX lesson challenge - Andrew's arrow function shorthand

--- complete-react-projects/indecision-app/src/app.js
+++ complete-react-projects/indecision-app/src/app.js
@@ -36,16 +36,9 @@ const render = () => {
         <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
         <p>{app.options.length}</p>
         <button onClick={onRemoveAll}>Remove All</button>
-        { /*
-            numbers.map((number) => {
-            return <p key={number}>Number: {number}</p>;
-          })
-        */ }
         <ol>
         {
-          app.options.map((option) => {
-            return <li key={option}>{option}</li>;
-          })
+          app.options.map((option) => <li key={option}>{option}</li>)
         }
         </ol>
         <form onSubmit={onFormSubmit}>
