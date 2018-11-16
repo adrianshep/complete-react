2f8dc5d3397c47e6327213c2673d2c12ac1db59e
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-11-16 16:22:58 -0500

    build it: visibility toggle - experimenting with function

--- complete-react-projects/indecision-app/src/playground/build-it-visible.js
+++ complete-react-projects/indecision-app/src/playground/build-it-visible.js
@@ -1,10 +1,19 @@
+const hide = () => {
+  <div>
+    <h1>Visibility Toggle</h1>
+    <button>Hide details</button>
+    <p>Hey. These are some details you can now see!</p>
+  </div>
+  render();
+};
+
 const appRoot = document.getElementById('app');
 
 const render = () => {
   const template = (
     <div>
       <h1>Visibility Toggle</h1>
-      <button>Show button</button>
+      <button onClick={hide}>Show details</button>
     </div>
   );
 
