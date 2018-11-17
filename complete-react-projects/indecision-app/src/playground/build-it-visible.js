f7346c7f7ae98570389be87de751d14755e34677
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-11-17 13:14:19 -0500

    build it: visibility toggle - Andrew's solution

--- complete-react-projects/indecision-app/src/playground/build-it-visible.js
+++ complete-react-projects/indecision-app/src/playground/build-it-visible.js
@@ -1,23 +1,65 @@
-const hide = () => {
-  <div>
-    <h1>Visibility Toggle</h1>
-    <button>Hide details</button>
-    <p>Hey. These are some details you can now see!</p>
-  </div>
+let visibility = false;
+
+const toggleVisibility = () => {
+  visibility = !visibility;
   render();
 };
 
-const appRoot = document.getElementById('app');
-
 const render = () => {
-  const template = (
+  const jsx =  (
     <div>
       <h1>Visibility Toggle</h1>
-      <button onClick={hide}>Show details</button>
+      <button onClick={toggleVisibility}>
+        {visibility ? 'Hide details' : 'Show details'}
+      </button>
+      {visibility && (
+        <div>
+          <p>Hey. These are some details you can now see!</p>
+        </div>
+      )}
     </div>
   );
 
-  ReactDOM.render(template, appRoot);
+  ReactDOM.render(jsx, document.getElementById('app'));
 };
 
 render();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const toggle = () => {
+//   <button>{'Hide details' ? 'Show details' : 'Hide details'}</button>
+//   render();
+// };
+//
+// const appRoot = document.getElementById('app');
+//
+// const render = () => {
+//   const template = (
+//     <div>
+//     <div>
+//       <h1>Visibility Toggle</h1>
+//       <button=onClick{toggle}>Show details</button>
+//       <p>Hey. These are some details you can now see!</p>
+//     </div>
+//     </div>
+//   );
+//
+//   ReactDOM.render(template, appRoot);
+// };
+//
+// render();
