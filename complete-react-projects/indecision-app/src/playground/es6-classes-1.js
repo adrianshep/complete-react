b80ee59c7107075c1a8564540935adf7edda169e
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-11-19 20:57:13 -0500

    es6-classes-1 lesson challenge - set up constructor and method

--- complete-react-projects/indecision-app/src/playground/es6-classes-1.js
+++ complete-react-projects/indecision-app/src/playground/es6-classes-1.js
@@ -9,8 +9,24 @@ class Person {
   }
 }
 
+class PersonAge {
+  constructor(name = 'Anonymous', age = 0) {
+    this.name = name;
+    this.age = age;
+  }
+  getDescription() {
+    return `${this.name} is ${this.age} year(s) old.`;
+  }
+}
+
 const me = new Person('Dennis Zoriani');
 console.log(me.getGreeting());
 
 const other = new Person();
 console.log(other.getGreeting());
+
+const him = new PersonAge('Dennis Zoriani', 15);
+console.log(him.getDescription());
+
+const anon = new PersonAge();
+console.log(anon.getDescription());
