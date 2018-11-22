5f6ad8b781819241069ab9d12a04572ce0e35fc0
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-11-21 23:37:49 -0500

    creating a react component lesson - header

--- complete-react-projects/indecision-app/src/app.js
+++ complete-react-projects/indecision-app/src/app.js
@@ -1,58 +1,14 @@
-console.log('App.js is running!');
+class Header extends React.Component {
+  render() {
+    return <p>This is from Header</p>;
+  }
+}
 
-  const app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer',
-    options: []
-  };
+const jsx = (
+  <div>
+    <h1>Title</h1>
+    <Header />
+  </div>
+);
 
-  const onRemoveAll = () => {
-    app.options = [];
-    render();
-  };
-
-  const onMakeDecision = () => {
-    const randomNum = Math.floor(Math.random() * app.options.length);
-    const option = app.options[randomNum];
-    alert(option);
-  };
-
-  const onFormSubmit = (e) => {
-    e.preventDefault();
-
-    const option = e.target.elements.option.value;
-
-    if (option) {
-      app.options.push(option);
-      e.target.elements.option.value = '';
-      // call render function here after options array is added to
-      render();
-    }
-  };
-
-const appRoot = document.getElementById('app');
-
-const render = () => {
-    const template = (
-      <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
-        <button onClick={onRemoveAll}>Remove All</button>
-        <ol>
-        {
-          app.options.map((option) => <li key={option}>{option}</li>)
-        }
-        </ol>
-        <form onSubmit={onFormSubmit}>
-          <input type="text" name="option"/>
-          <button>Add Option</button>
-        </form>
-      </div>
-    );
-
-  ReactDOM.render(template, appRoot);
-};
-// and call render function here
-render();
+ReactDOM.render(jsx, document.getElementById('app'));
