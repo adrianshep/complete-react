a8797f38f7266392701c3d1c5faee51753c77c25
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-12-14 18:56:24 -0500

    es6 import/export lesson challenge

--- complete-react-projects/indecision-app/src/app.js
+++ complete-react-projects/indecision-app/src/app.js
@@ -1,6 +1,11 @@
+import './person.js';
+import { isAdult, canDrink } from './person.js';
 // import './utils.js';
-import { square, add } from './utils.js';
+// import { square, add } from './utils.js';
+//
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(100, 23));
 
-console.log('app.js is running');
-console.log(square(4));
-console.log(add(100, 23));
+console.log(isAdult(18));
+console.log(canDrink(18));
---
+++ complete-react-projects/indecision-app/src/person.js
@@ -0,0 +1,19 @@
+console.log('person.js is running');
+
+const isAdult = (age) => {
+  if (age >= 18) {
+    console.log('true');
+  } else {
+    console.log('false');
+  }
+};
+
+const canDrink = (age) => {
+  if (age >= 21) {
+    console.log('true');
+  } else {
+    console.log('false');
+  }
+}
+
+export { isAdult, canDrink };
