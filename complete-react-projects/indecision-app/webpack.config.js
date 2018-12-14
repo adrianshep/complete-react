6776a5bbcee711e19ab7f05fb538bfdeeca6cb0a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-12-14 12:42:43 -0500

    additional file changes

--- complete-react-projects/indecision-app/webpack.config.js
+++ complete-react-projects/indecision-app/webpack.config.js
@@ -1,7 +1,4 @@
 const path = require('path');
-// entry -> output
-// relative path:  ~/complete-react/complete-react-projects/indecision-app
-// or: /Users/adrianshepelavey/complete-react/complete-react-projects/indecision-app
 
 module.exports = {
   entry: './src/app.js',
