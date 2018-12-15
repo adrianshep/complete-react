658b10510d9d3f0fa8cc8aab8d30af091cadcdea
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-12-15 15:35:24 -0500

    default exports lesson challenge - Andrew's solution - default naming doesn't have to match between files

--- complete-react-projects/indecision-app/src/person.js
+++ complete-react-projects/indecision-app/src/person.js
@@ -1,5 +1,5 @@
 const isAdult = (age) => age >= 18;
 const canDrink = (age) => age >= 21;
-const isSenior = (age) => age >= 65;
+const isSeniorCitizen = (age) => age >= 65;
 
-export { isAdult, canDrink, isSenior as default };
+export { isAdult, canDrink, isSeniorCitizen as default };
