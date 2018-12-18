ce676e276525fb75467c7fce1f7051034f12cdeb
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-12-18 16:27:30 -0500

    one component per file lesson challenge - setting up Header and Action

--- complete-react-projects/indecision-app/src/app.js
+++ complete-react-projects/indecision-app/src/app.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AddOption from './components/AddOption';
 import Option from './components/Option';
+import Header from './components/Header';
+import Action from './components/Action';
 
 class IndecisionApp extends React.Component {
   constructor(props) {
@@ -81,32 +83,6 @@ class IndecisionApp extends React.Component {
     );
   }
 }
-
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.title}</h1>
-      {props.subtitle && <h2>{props.subtitle}</h2>}
-    </div>
-  );
-};
-
-Header.defaultProps = {
-  title: 'Indecision'
-};
-
-const Action = (props) => {
-  return (
-    <div>
-      <button
-        onClick={props.handlePick}
-        disabled={!props.hasOptions}
-      >
-        What should I do?
-      </button>
-    </div>
-  );
-};
 
 const Options = (props) => {
   return (
