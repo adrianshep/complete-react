0d327e86a6ddbf25a738394a93ae3bc18a55ef38
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-12-24 12:14:39 -0500

    setting up react-modal lesson challenge

--- complete-react-projects/indecision-app/src/components/IndecisionApp.js
+++ complete-react-projects/indecision-app/src/components/IndecisionApp.js
@@ -22,7 +22,9 @@ export default class IndecisionApp extends React.Component {
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
-    alert(option);
+    this.setState((option) => {
+      OptionModal.isOpen();
+    });
   };
   handleAddOption = (option) => {
     if (!option) {
