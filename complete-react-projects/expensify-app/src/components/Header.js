0e2439dfdc819147a588a8b7cef52f4fa1ae4708
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-01-14 16:34:45 -0500

    query strings and url parameters lesson

--- complete-react-projects/expensify-app/src/components/Header.js
+++ complete-react-projects/expensify-app/src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
     <h1>Expensify</h1>
     <div><NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink></div>
     <div><NavLink to="/create" activeClassName="is-active">Add Expense</NavLink></div>
-    <div><NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink></div>
     <div><NavLink to="/help" activeClassName="is-active">Help</NavLink></div>
   </header>
 );
