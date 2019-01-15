9263c82590bd2d546f372b90bdf08a4675ca7f34
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-01-15 15:37:54 -0500

    build it: router for portfolio site lesson challenge - home, portfolio, contact pages

---
+++ complete-react-projects/portfolio-site/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => (
+  <header>
+    <h1>Portfolio</h1>
+    <div><NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink></div>
+    <div><NavLink to="/portfolio" activeClassName="is-active">Portfolio</NavLink></div>
+    <div><NavLink to="/contact" activeClassName="is-active">Contact</NavLink></div>
+  </header>
+);
+
+export default Header;
---
+++ complete-react-projects/portfolio-site/src/components/PortfolioPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PortfolioPage = () => (
+  <div>
+    <h1>My Work</h1>
+    <p>Check out the following things I've done:</p>
+    <p>Item One</p>
+    <p>Item Two</p>
+  </div>
+);
+
+export default PortfolioPage;
