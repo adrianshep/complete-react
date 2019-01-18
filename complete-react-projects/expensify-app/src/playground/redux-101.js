12be756e99ca45e0362923718be599ea0bc359fe
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-01-18 12:30:37 -0500

    dispatching actions lesson

--- complete-react-projects/expensify-app/src/playground/redux-101.js
+++ complete-react-projects/expensify-app/src/playground/redux-101.js
@@ -1,7 +1,33 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+    return {
+      count: state.count + 1
+    };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+});
+
+console.log(store.getState());
+
+// I'd like to increment the count
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'DECREMENT'
+});
+
+store.dispatch({
+  type: 'DECREMENT'
 });
 
 console.log(store.getState());
