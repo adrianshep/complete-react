d91a8a7d8aca10e1501d9916b913e3a84d96d24a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-01-20 13:21:16 -0500

    es6 object destructuring lesson challenge

--- complete-react-projects/expensify-app/src/playground/destructuring.js
+++ complete-react-projects/expensify-app/src/playground/destructuring.js
@@ -1,16 +1,28 @@
-const person = {
-  name: 'Tom',
-  age: 19,
-  location: {
-    city: 'New Meeting',
-    temp: 88
+// const person = {
+//   name: 'Tom',
+//   age: 19,
+//   location: {
+//     city: 'New Meeting',
+//     temp: 88
+//   }
+// };
+//
+// const { name: firstName = 'Anonymous', age } = person;
+// console.log(`${firstName} is ${age}.`);
+//
+// const { city, temp: temperature } = person.location;
+// if (city && temperature) {
+//     console.log(`It's ${temperature} in ${city}.`);
+// }
+
+const book = {
+  title: 'Ego is the Enemy',
+  author: 'Ryan Holiday',
+  publisher: {
+    name: 'Penguin'
   }
 };
 
-const { name: firstName = 'Anonymous', age } = person;
-console.log(`${firstName} is ${age}.`);
+const { name: publisherName = 'Self-Published' } = book.publisher;
 
-const { city, temp: temperature } = person.location;
-if (city && temperature) {
-    console.log(`It's ${temperature} in ${city}.`);
-}
+console.log(publisherName);
