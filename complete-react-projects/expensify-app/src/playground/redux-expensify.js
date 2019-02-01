a4d8a4640945ac2675428911b230f10e6738dc0b
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-02-01 16:53:13 -0500

    sorting redux data lesson challenge - sortBy amount

--- complete-react-projects/expensify-app/src/playground/redux-expensify.js
+++ complete-react-projects/expensify-app/src/playground/redux-expensify.js
@@ -149,6 +149,8 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
   }).sort((a, b) => {
     if (sortBy === 'date') {
       return a.createdAt < b.createdAt ? 1 : -1;
+    } else if (sortBy === 'amount') {
+      return a.amount > b.amount ? -1 : 1;
     }
   });
 };
@@ -177,7 +179,7 @@ const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 30
 // store.dispatch(setTextFilter('rent'));
 // store.dispatch(setTextFilter());
 //
-// store.dispatch(sortByAmount());
+store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 
 // store.dispatch(setStartDate(0));
