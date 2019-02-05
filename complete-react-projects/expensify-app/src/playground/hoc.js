efaf01fd895b21e275b01e9c2a089b0ea6d53d0a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-02-05 13:15:30 -0500

    the higher order component lesson challenge - Andrew's solution

--- complete-react-projects/expensify-app/src/playground/hoc.js
+++ complete-react-projects/expensify-app/src/playground/hoc.js
@@ -23,19 +23,21 @@ const withAdminWarning = (WrappedComponent) => {
   );
 };
 
+// requireAuthentication
 const requireAuthentication = (WrappedComponent) => {
   return (props) => (
     <div>
-      {!props.isAuthenticated && <p>Please log in to view the info.</p>}
-      <WrappedComponent {...props}/>
+      {props.isAuthenticated ? (
+        <WrappedComponent {...props}/>
+      ) : (
+        <p>Please log in to view the info.</p>
+      )}
     </div>
   );
 };
-
-// requireAuthentication
 
 const AdminInfo = withAdminWarning(Info);
 const AuthInfo = requireAuthentication(Info);
 
 // ReactDOM.render(<AdminInfo isAdmin={true} info="These are the details" />, document.getElementById('app'));
-ReactDOM.render(<AuthInfo isAuthenticated={false} info="These are the details" />, document.getElementById('app'));
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="These are the details" />, document.getElementById('app'));
