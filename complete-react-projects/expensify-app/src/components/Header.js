d3a4891bcfe370e600453ebdee0b77916be266d2
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-02-14 12:11:23 -0500

    creating expense add/edit form

--- complete-react-projects/expensify-app/src/components/Header.js
+++ complete-react-projects/expensify-app/src/components/Header.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
   <header>
     <h1>Expensify</h1>
-    <div><NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink></div>
-    <div><NavLink to="/create" activeClassName="is-active">Add Expense</NavLink></div>
+    <div><NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink></div>
+    <div><NavLink to="/create" activeClassName="is-active">Create Expense</NavLink></div>
     <div><NavLink to="/help" activeClassName="is-active">Help</NavLink></div>
   </header>
 );
