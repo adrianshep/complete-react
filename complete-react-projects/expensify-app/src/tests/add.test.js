bd364951baa8e9d177f9c0d7db89632359280c2b
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-02-26 13:00:14 -0500

    setting up Jest lesson challenge

--- complete-react-projects/expensify-app/src/tests/add.test.js
+++ complete-react-projects/expensify-app/src/tests/add.test.js
@@ -1,6 +1,12 @@
 const add = (a, b) => a + b;
+const generateGreeting = () => `Hello ${name}!`;
 
 test('should add two numbers', () => {
   const result = add(3, 4);
   expect(result).toBe(7);
+});
+
+test('should generate greeting with name', () => {
+  name = 'Dennis';
+  expect(name).toBe('Dennis');
 });
