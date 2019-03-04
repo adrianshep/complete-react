f0a1c4e64b27ac979221f15c71b4123d0e25dccc
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-03-04 12:43:34 -0500

    testing filters action generators lesson challenge - setTextFilter, sortByDate, sortByAmount

--- complete-react-projects/expensify-app/src/tests/actions/filters.test.js
+++ complete-react-projects/expensify-app/src/tests/actions/filters.test.js
@@ -1,5 +1,35 @@
 import moment from 'moment';
-import { setStartDate, setEndDate } from '../../actions/filters';
+import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../../actions/filters';
+
+test('should generate set text filter action object with text', () => {
+  const action = setTextFilter(text = '');
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text: text = ''
+  });
+});
+
+test('should generate set text filter action object without text', () => {
+  const action = setTextFilter(text = '');
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text: ''
+  });
+});
+
+test('should generate sort by date action object', () => {
+  const action = sortByDate();
+  expect(action).toEqual({
+    type: 'SORT_BY_DATE'
+  });
+});
+
+test('should generate sort by amount action object', () => {
+  const action = sortByAmount();
+  expect(action).toEqual({
+    type: 'SORT_BY_AMOUNT'
+  });
+});
 
 test('should generate set start date action object', () => {
   const action = setStartDate(moment(0));
