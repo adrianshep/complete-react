63e018e44dfd34ccd58d3c5dc9a3f2e92374c988
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-03-06 12:59:17 -0500

    testing expenses selector lesson challenge - filter by endDate, sort by date, amount - Andrew's solution

--- complete-react-projects/expensify-app/src/tests/selectors/expenses.test.js
+++ complete-react-projects/expensify-app/src/tests/selectors/expenses.test.js
@@ -48,10 +48,10 @@ test('should filter by endDate', () => {
     text: '',
     sortBy: 'date',
     startDate: undefined,
-    endDate: moment(0)
+    endDate: moment(0).add(2, 'days')
   };
   const result = selectExpenses(expenses, filters);
-  expect(result).toEqual([expenses[2], expenses[0]]);
+  expect(result).toEqual([expenses[0], expenses[1]]);
 });
 
 test('should sort by date', () => {
@@ -62,16 +62,16 @@ test('should sort by date', () => {
     endDate: undefined
   };
   const result = selectExpenses(expenses, filters);
-  expect(result).toEqual([expenses[2], expenses[0]]);
+  expect(result).toEqual([expenses[2], expenses[0], expenses[1]]);
 });
 
 test('should sort by amount', () => {
   const filters = {
     text: '',
-    sortBy: amount,
+    sortBy: 'amount',
     startDate: undefined,
     endDate: undefined
   };
   const result = selectExpenses(expenses, filters);
-  expect(result).toEqual([expenses[2], expenses[0]]);
+  expect(result).toEqual([expenses[1], expenses[2], expenses[0]]);
 });
