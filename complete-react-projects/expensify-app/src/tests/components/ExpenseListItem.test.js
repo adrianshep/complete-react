1acaa2701edd928b338319ad725ee818197ebbde
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-03-14 13:27:26 -0400

    snapshot testing with dynamic components lesson challenge - ExpenseListItem - Andrew's solution

--- complete-react-projects/expensify-app/src/tests/components/ExpenseListItem.test.js
+++ complete-react-projects/expensify-app/src/tests/components/ExpenseListItem.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { ExpenseListItem } from '../../components/ExpenseListItem';
+import ExpenseListItem from '../../components/ExpenseListItem';
 import expenses from '../fixtures/expenses';
 
-test('should render ExpenseListItem with an expense', (expense) => {
-  const wrapper = shallow(<ExpenseListItem key={expense.id} />);
+test('should render ExpenseListItem correctly', () => {
+  const wrapper = shallow(<ExpenseListItem {...expenses[0]} />);
   expect(wrapper).toMatchSnapshot();
 });
