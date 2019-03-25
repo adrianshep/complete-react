b97ad4f3577f9147b28c5fad12a1603b93d12f81
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-03-25 12:39:43 -0400

    testing AddExpensePage lesson - using beforeEach - correction

--- complete-react-projects/expensify-app/src/tests/components/AddExpensePage.test.js
+++ complete-react-projects/expensify-app/src/tests/components/AddExpensePage.test.js
@@ -16,9 +16,6 @@ test('should render AddExpensePage correctly', () => {
 });
 
 test('should handle onSubmit', () => {
-  const onSubmit = jest.fn();
-  const history = { push: jest.fn() };
-  const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
   expect(history.push).toHaveBeenLastCalledWith('/');
   expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
