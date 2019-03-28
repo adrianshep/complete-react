49f44ee61f3d3e22ba0cc7df2d5ce2af345b3cd6
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-03-27 22:09:32 -0400

    testing EditExpensePage lesson challenge - modifying EditExpensePage

--- complete-react-projects/expensify-app/src/components/EditExpensePage.js
+++ complete-react-projects/expensify-app/src/components/EditExpensePage.js
@@ -3,25 +3,31 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { editExpense, removeExpense } from '../actions/expenses';
 
-const EditExpensePage = (props) => {
-  return (
-    <div>
-      <ExpenseForm
-        expense={props.expense}
-        onSubmit={(expense) => {
-          props.dispatch(editExpense(props.expense.id, expense));
+export class EditExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.editExpense(expense);
+    this.props.history.push('/');
+  };
+  render() {
+    return (
+      <div>
+        <ExpenseForm
+          expense={props.expense}
+          onSubmit={(expense) => {
+            props.dispatch(editExpense(props.expense.id, expense));
+            props.history.push('/');
+          }}
+        />
+        <button onClick={() => {
+          props.dispatch(removeExpense({ id: props.expense.id }));
           props.history.push('/');
-        }}
-      />
-      <button onClick={() => {
-        props.dispatch(removeExpense({ id: props.expense.id }));
-        props.history.push('/');
-      }}>Remove</button>
-    </div>
-  );
-};
+        }}>Remove</button>
+      </div>
+    );
+  }
+}
 
-const mapStateToProps = (state, props) => {
+const mapStateToProps = (dispatch) => {
   return {
     expense: state.expenses.find((expense) => expense.id === props.match.params.id)
   };
