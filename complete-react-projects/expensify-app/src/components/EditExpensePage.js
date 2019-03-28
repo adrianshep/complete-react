5e48643a1a3febe15377eb8c3dc8bb003ceab247
Adrian Shepelavey <adrianshepelavey@gmail.com>
2019-03-28 13:00:54 -0400

    testing EditExpensePage lesson challenge - modifying EditExpensePage - Andrew's solution

--- complete-react-projects/expensify-app/src/components/EditExpensePage.js
+++ complete-react-projects/expensify-app/src/components/EditExpensePage.js
@@ -5,32 +5,33 @@ import { editExpense, removeExpense } from '../actions/expenses';
 
 export class EditExpensePage extends React.Component {
   onSubmit = (expense) => {
-    this.props.editExpense(expense);
+    this.props.editExpense(this.props.expense.id, expense);
+    this.props.history.push('/');
+  };
+  onRemove = () => {
+    this.props.removeExpense({ id: this.props.expense.id });
     this.props.history.push('/');
   };
   render() {
     return (
       <div>
         <ExpenseForm
-          expense={props.expense}
-          onSubmit={(expense) => {
-            props.dispatch(editExpense(props.expense.id, expense));
-            props.history.push('/');
-          }}
+          expense={this.props.expense}
+          onSubmit={this.onSubmit}
         />
-        <button onClick={() => {
-          props.dispatch(removeExpense({ id: props.expense.id }));
-          props.history.push('/');
-        }}>Remove</button>
+        <button onClick={this.onRemove}>Remove</button>
       </div>
     );
   }
-}
-
-const mapStateToProps = (dispatch) => {
-  return {
-    expense: state.expenses.find((expense) => expense.id === props.match.params.id)
-  };
 };
 
-export default connect(mapStateToProps)(EditExpensePage);
+const mapStateToProps = (state, props) => ({
+  expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+});
+
+const mapDispatchToProps = (dispatch, props) => ({
+  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+  removeExpense: (data) => dispatch(removeExpense(data))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
